5846aea8a3983252c1320fb8dffc7df4c8b2daa7
Marwan <marwan.boubchir@outlook.fr>
2021-11-29 11:16:11 +0100

    Seat

--- src/main/java/fr/esgi/tp1/Seat.java
+++ src/main/java/fr/esgi/tp1/Seat.java
@@ -1,5 +1,41 @@
 package fr.esgi.tp1;
 
 public class Seat {
+    private int id;
+    private Train train;
+    private boolean statut;
 
+    public Seat(int id, Train leTrain, boolean statut ){
+        this.id = id;
+        this.train = leTrain;
+        this.statut = statut;
+    }
+
+    public Seat(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public boolean isStatut() {
+        return statut;
+    }
+
+    public void setStatut(boolean statut) {
+        this.statut = statut;
+    }
 }
